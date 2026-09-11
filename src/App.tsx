65cb8c2ad4784ef54f3c0c8da163c1ebd0b09e1e
Rakib304832 <h.rakib0000000000@gmail.com>
2026-09-11 14:42:56 +0600

    header complite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from "./compunent/Navber.jsx"
+import Banner from "./compunent/Banner.js"
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
